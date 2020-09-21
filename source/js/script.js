2af516e6a2c27455400be721e5ad6c69428221ca
Volkov85 <xplz2007@gmail.com>
2020-09-21 13:28:41 +0600

    Доработка главной

--- source/js/script.js
+++ source/js/script.js
@@ -62,11 +62,104 @@ let fullpage = new fullpage(`#fullpage`, {
   licenseKey: `YOUR_KEY_HERE`,
   recordHistory: false,
   scrollOverflow: true,
+  afterRender() {
+    // Появление соц.иконок и стрелок слайдера
+    let heroSocials = document.querySelector(`.hero__social`);
+    let heroSliderControls = document.querySelector(`.hero__slider-control`);
+
+    heroSocials.classList.add(`hero__social--shown`);
+    heroSliderControls.classList.add(`hero__slider-control--shown`);
+  },
   onLeave(origin, destination) {
     if (destination.isFirst) {
-      header.classList.remove(`header--hidden`);
+      header.classList.remove(`header--small`);
     } else {
-      header.classList.add(`header--hidden`);
+      header.classList.add(`header--small`);
     }
   }
 });
+
+/**
+ * Оживление формы
+ */
+let partnershipInputs = document.querySelectorAll(`.partnership__form-field`);
+
+for (let i = 0; i < partnershipInputs.length; i++) {
+  partnershipInputs[i].firstChild.addEventListener(`input`, () => {
+    if (partnershipInputs[i].firstChild.value) {
+      partnershipInputs[i].firstChild.classList.add(`filled`);
+    } else {
+      partnershipInputs[i].firstChild.classList.remove(`filled`);
+    }
+  });
+}
+
+/**
+ * Оживление FAQ
+ */
+let faqQuestions = document.querySelectorAll(`.faq__qa-question`);
+let faqAnswers = document.querySelectorAll(`.faq__qa-answer`);
+
+let faqPopup = document.querySelector(`.popup--qa-answer`);
+let faqPopupClose = faqPopup.querySelector(`.popup__close`);
+let faqPopupQuestion = faqPopup.querySelector(`.qa-answer__title`);
+let faqPopupAnswer = faqPopup.querySelector(`.qa-answer__text`);
+let overlay = document.querySelector(`.overlay`);
+
+for (let i = 0; i < faqQuestions.length; i++) {
+  faqQuestions[i].firstChild.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+
+    faqPopupQuestion.textContent = faqQuestions[i].firstChild.textContent;
+    faqPopupAnswer.textContent = faqAnswers[i].textContent;
+
+    faqPopup.classList.add(`popup--shown`);
+    overlay.classList.add(`overlay--shown`);
+  });
+}
+
+faqPopupClose.addEventListener(`click`, (evt) => {
+  evt.preventDefault();
+  removeAnimationClassShown(faqPopup, `popup--shown`);
+  overlay.classList.remove(`overlay--shown`);
+});
+
+overlay.addEventListener(`click`, () => {
+  let shownPopup = document.querySelector(`.popup--shown`);
+
+  removeAnimationClassShown(shownPopup, `popup--shown`);
+  overlay.classList.remove(`overlay--shown`);
+});
+
+/**
+ * Закрытие панели cookies
+ */
+let cookies = document.querySelector(`.cookies`);
+let cookiesBtn = cookies.querySelector(`.button`);
+
+cookiesBtn.addEventListener(`click`, (evt) => {
+  evt.preventDefault();
+  cookies.remove();
+});
+
+/**
+ * Скрытие элемента (удаляем класс &--shown)
+ */
+function removeAnimationClassShown(el, cls) {
+  resetCSSAnimation(el, cls);
+  el.style.animationDirection = `reverse`;
+
+  window.setTimeout(() => {
+    el.classList.remove(cls);
+    el.removeAttribute(`style`);
+  }, 300);
+}
+
+/**
+ * Сброс CSS-анимации
+ */
+function resetCSSAnimation(el, cls) {
+  el.classList.remove(cls);
+  void el.offsetWidth;
+  el.classList.add(cls);
+}
